cdc8b48dd58642a5032da8c56dc68d1e600e4966
2129 Student <ryanmichaelheuer2@gmail.com>
2022-10-18 12:33:51 -0500

    add quaternions.py

---
+++ quaternions.py
@@ -0,0 +1,31 @@
+import math
+
+def matrixToQuat(m):
+    r11 = m[0][0]; r12 = m[0][1]; r13 = m[0][2]
+    r21 = m[1][0]; r22 = m[1][1]; r23 = m[1][2]
+    r31 = m[2][0]; r32 = m[2][1]; r33 = m[2][2]
+
+    q0 = math.sqrt((1 + r11 + r22 + r33) / 4)
+    q1 = math.sqrt((1 + r11 - r22 - r33) / 4)
+    q2 = math.sqrt((1 - r11 + r22 - r33) / 4)
+    q3 = math.sqrt((1 - r11 - r22 + r33) / 4)
+
+    if q0 > q1 and q0 > q2 and q0 > q3:
+        q1 = (r32 - r23) / (4 * q0)
+        q2 = (r13 - r31) / (4 * q0)
+        q3 = (r21 - r12) / (4 * q0)
+    elif q1 > q0 and q1 > q2 and q1 > q3:
+        q0 = (r32 - r23) / (4 * q1)
+        q2 = (r12 + r21) / (4 * q1)
+        q3 = (r13 + r31) / (4 * q1)
+    elif q2 > q0 and q2 > q1 and q2 > q3:
+        q0 = (r13 - r31) / (4 * q2)
+        q1 = (r12 + r21) / (4 * q2)
+        q3 = (r23 + r32) / (4 * q2)
+    elif q3 > q0 and q3 > q1 and q3 > q2:
+        q0 = (r21 - r12) / (4 * q3)
+        q1 = (r13 + r31) / (4 * q3)
+        q2 = (r23 + r32) / (4 * q3)
+
+    return (q0, q1, q2, q3)
+
