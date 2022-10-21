e1f6b85cf43383efcaf54980a2de7ed13fbe7601
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-10-21 10:11:59 -0500

    Added run script to make user side easier (Not done)

---
+++ run.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# Edit this file to define the behavior
+# of your camera system
+
+echo "Starting AprilTag tracker"
+
+# Settings for each camera
+# 1. Camera params
+# 2. Camera location on robot
+# 3. Camera ID
