f58c6c07e617bc358beb802e13ba423b8d47e443
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-10-28 10:02:22 -0500

    Pasted cube gui

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
 
 DRAW_GUI = True
 
-def drawBoundingBox(overlay, detector_result):
+def draw_bounding_box(overlay, detector_result):
     # Copied from AprilTag source
 
     # Extract the bounding box (x, y)-coordinates for the AprilTag
@@ -32,3 +33,50 @@ def drawBoundingBox(overlay, detector_result):
     tagId = str(detector_result.tag_id)
     cv2.putText(overlay, tagId, ((cX, cY)),
     cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2)
+
+# Not done TODO: Finish
+def draw_cube(overlay, camera_params, tag_size, pose, z_sign=1):
+
+    opoints =  np.array([
+        -1, -1, 0,
+         1, -1, 0,
+         1,  1, 0,
+        -1,  1, 0,
+        -1, -1, -2*z_sign,
+         1, -1, -2*z_sign,
+         1,  1, -2*z_sign,
+        -1,  1, -2*z_sign,
+    ]).reshape(-1, 1, 3) * 0.5*tag_size
+
+    edges = np.array([
+        0, 1,
+        1, 2,
+        2, 3,
+        3, 0,
+        0, 4,
+        1, 5,
+        2, 6,
+        3, 7,
+        4, 5,
+        5, 6,
+        6, 7,
+        7, 4
+    ]).reshape(-1, 2)
+
+    fx, fy, cx, cy = camera_params
+
+    K = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape(3, 3)
+
+    rvec, _ = cv2.Rodrigues(pose[:3,:3])
+    tvec = pose[:3, 3]
+
+    dcoeffs = np.zeros(5)
+
+    ipoints, _ = cv2.projectPoints(opoints, rvec, tvec, K, dcoeffs)
+
+    ipoints = np.round(ipoints).astype(int)
+
+    ipoints = [tuple(pt) for pt in ipoints.reshape(-1, 2)]
+
+    for i, j in edges:
+        cv2.line(overlay, ipoints[i], ipoints[j], (0, 255, 0), 1, 16)
