bf1ec296b4e51c47365d76781a5c57dc2b1b72ae
pheonixrobotics-desktop1 <pheonixrobotics.desktop1@gmail.com>
2022-12-05 18:07:00 -0500

    Fix sudo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+if [[ `id -u` -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo "[INFO] Starting setup"
 
