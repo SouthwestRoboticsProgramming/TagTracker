c67084cbcb4dd7d24d6312859031cce4952950c2
pheonixrobotics-desktop1 <pheonixrobotics.desktop1@gmail.com>
2022-12-05 18:02:02 -0500

    Fix Sudo

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 echo "[INFO] Starting setup"
 
-if [ "$EUID" -ne 0 ] then
-    yes | sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 cmake python3 python3-pip
-else
-    yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 cmake python3 python3-pip
-fi 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 cmake python3 python3-pip
 
 echo "[INFO] Installing dependencies"
 
