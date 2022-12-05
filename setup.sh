9b1f0f636affc622eb950a9fc1f371916fd52170
pheonixrobotics-desktop1 <pheonixrobotics.desktop1@gmail.com>
2022-12-05 18:05:12 -0500

    Fix sudo

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "[INFO] Starting setup"
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+echo "[INFO] Starting setup"
 
 yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 cmake python3 python3-pip
 
