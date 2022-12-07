090844c117b35929d922721e25244833664caa73
pheonixrobotics-desktop1 <pheonixrobotics.desktop1@gmail.com>
2022-12-07 17:21:06 -0500

    Fix setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ if [ `id -u` -ne 0 ] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo "[INFO] Starting setup"
 
-yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 cmake python3 python3-pip
+yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 libopencv-dev cmake python3 python3-pip python3-opencv
 
 echo "[INFO] Installing dependencies"
 
-pip install opencv-contrib-python apriltag pynetworktables
+pip install apriltag pynetworktables
 
 echo "[INFO] Finished setup"
