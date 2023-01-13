bc0c4b1f519859938064b0e7558c5f37601d56ac
drakeerv <drakeerv@outlook.com>
2023-01-13 18:41:20 -0500

    Change requirements and fix install

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ fi
 
 echo "[INFO] Starting setup"
 
-yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+yes | apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python3 python3-pip cmake
 
-echo "[INFO] Created environment for python"
+echo "[INFO] Installed packages"
 
 pip install -r requirements.txt
 
