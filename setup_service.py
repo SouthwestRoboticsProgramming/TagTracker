d081b2a814b0ba0a73cb701096c57a187c846cca
drakeerv <drakeerv@outlook.com>
2023-01-13 18:04:28 -0500

    Fix base path

--- setup_service.py
+++ setup_service.py
@@ -1,9 +1,10 @@
 import os
 
-script_path = os.path.join(os.path.dirname(__file__), "src/main.py")
+base_path = os.path.dirname(__file__)
+script_path = "src/main.py"
 
 with open("apriltag_detector.service", "r") as source:
-    source_content = map(lambda x: x.replace("%s", script_path), source.readlines())
+    source_content = map(lambda x: x.replace("%s", script_path).replace(base_path), source.readlines())
 
     with open("/etc/systemd/system/apriltag_detector.service", "w") as destination:
         destination.writelines(source_content)
