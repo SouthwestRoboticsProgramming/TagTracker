ac4076ae8a1c2ee8c2eb08228b907286faa1e56a
drakeerv <drakeerv@outlook.com>
2023-01-13 17:17:29 -0500

    Fix description and root detector

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if ! [ $(id -u) = 0 ]; then
+   echo "Please run as root!"
+   exit 1
 fi
 
 echo "[INFO] Starting setup"
