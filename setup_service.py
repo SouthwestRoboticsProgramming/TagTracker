11de84127651e8f57082db8b4254bc2ecd2d3833
drakeerv <drakeerv@outlook.com>
2023-01-13 17:04:29 -0500

    Debugging service

--- setup_service.py
+++ setup_service.py
@@ -1,9 +1,12 @@
 import os
 
 script_path = os.path.join(os.path.dirname(__file__), "src/main.py")
+print(script_path)
 
 with open("apriltag_detector.service") as source:
     source_content = map(lambda x: x.replace("%s", script_path), source.readlines())
+    print(source_content)
 
     with open("/etc/systemd/system/apriltag_detector.service") as destination:
+        print("write")
         destination.writelines(source_content)
